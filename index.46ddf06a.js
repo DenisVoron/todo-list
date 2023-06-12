fc5a632347130d926c7e1d8ec91282843a31d54a
DenisVoron <97794799+DenisVoron@users.noreply.github.com>
2023-06-12 19:54:28 +0000

    Deploying to gh-pages from @ DenisVoron/todo-list@cf26c8b879157fdc0c50f97f9bea90a5e2c73f3a 🚀

---
+++ index.46ddf06a.js
@@ -0,0 +1,2 @@
+(()=>{const{height:e}=document.querySelector(".header").getBoundingClientRect();document.body.style.paddingTop=`${e}px`})();
+//# sourceMappingURL=index.46ddf06a.js.map
---
+++ index.ce0773ea.js
@@ -0,0 +1,2 @@
+!function(){var e;e=document.querySelector(".header").getBoundingClientRect().height,document.body.style.paddingTop="".concat(e,"px")}();
+//# sourceMappingURL=index.ce0773ea.js.map
