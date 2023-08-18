4e131dcad577644cfbec124e9b6df51c2a658786
DenisVoron <Denis.voronko@gmail.com>
2023-08-18 21:46:04 +0300

    add timer2

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,18 +1,24 @@
 const templateTimer = time => {
     return `<fieldset class="widgets-fieldset">
               <legend>Timer</legend>
-              <span class="js-timer-field">${time}</span>
-              <button class="button success pointer js-btn-start">start</button>
-              <button class="button danger pointer js-btn-stop">stop</button>
+              <span class="timer-field js-timer-field">${time}</span>
+              <button class="button timer success pointer js-btn-start">start</button>
+              <button class="button timer danger pointer js-btn-stop">stop</button>
+              <button class="button timer pointer js-btn-clear">clear</button>
             </fieldset>`;
 }
 
-
 export default class Timer {
     constructor({ time, selector }) {
-        this.date = Date.now();
-        this.time = time;
-        this.intervalId = 0;
+        this.time = 0;
+        this.startTime = time;
+        this.elapsedTime = 0;
+        this.currentTime = 0;
+        this.hrs = 0;
+        this.mins = 0;
+        this.secs = 0;
+
+        this.intervalId;
         this.refs = {};
 
         this.refsTimer = document.querySelector(selector);
@@ -22,9 +28,11 @@ export default class Timer {
 
         this.refs.btnStart = document.querySelector('.js-btn-start');
         this.refs.btnStop = document.querySelector('.js-btn-stop');
+        this.refs.btnClear = document.querySelector('.js-btn-clear');
 
-        this.refs.btnStop.addEventListener('click', this.timerStop.bind(this));
         this.refs.btnStart.addEventListener('click', this.timerStart.bind(this));
+        this.refs.btnStop.addEventListener('click', this.timerStop.bind(this));
+        this.refs.btnClear.addEventListener('click', this.timeClear.bind(this))
     }
 
     render(time) {
@@ -35,32 +43,45 @@ export default class Timer {
         if (this.intervalId) {
             return
         }
-        this.intervalId = setInterval(() => {
-            this.time += 1;
-            this.render(this.time);
-        }, 1000);
+
+        this.startTime = Date.now() - this.elapsedTime;
+
+        this.intervalId = setInterval(this.updateTime.bind(this), 75);
     }
 
     timerStop() {
         clearInterval(this.intervalId);
+        this.intervalId = 0;
     }
 
-    convertMs(ms) {
-        // Number of milliseconds per unit of time
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
+    timeClear() {
+        this.refs.refsField.textContent = `00:00:00`;
 
-        // Remaining days
-        const days = Math.floor(ms / day);
-        // Remaining hours
-        const hours = Math.floor((ms % day) / hour);
-        // Remaining minutes
-        const minutes = Math.floor(((ms % day) % hour) / minute);
-        // Remaining seconds
-        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+        clearInterval(this.intervalId);
+        this.intervalId = 0;
 
-        return { days, hours, minutes, seconds };
+        this.startTime = 0;
+        this.elapsedTime = 0;
+        this.currentTime = 0;
+    }
+
+    updateTime() {
+        this.elapsedTime = Date.now() - this.startTime;
+
+        this.secs = Math.floor((this.elapsedTime / 1000) % 60);
+        this.mins = Math.floor((this.elapsedTime / (1000 * 60)) % 60);
+        this.hrs = Math.floor((this.elapsedTime / (1000 * 60 * 60)) % 60);
+
+        this.secs = this.pad(this.secs);
+        this.mins = this.pad(this.mins);
+        this.hrs = this.pad(this.hrs);
+
+        this.time = `${this.hrs}:${this.mins}:${this.secs}`;
+        this.render(this.time)
+
+    }
+
+    pad(unit) {
+        return (("0") + unit).length > 2 ? unit : "0" + unit;
     }
 }
